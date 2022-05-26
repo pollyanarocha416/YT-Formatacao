d22d3c639e53aea99573e9a679f54c9ed0658ddf
Pollyana <pollyanarocha416@gmail.com>
2022-05-26 11:40:06 -0300

    Calcular Lucro

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+   class Formatacao
+   {
+       static void Main()
+       {
+        double valcompra = 5.50;
+        double valvenda;
+        double lucro = 0.1;
+        string produto = "Pastel";
+        valvenda = valcompra + (valcompra * lucro);
+
+        Console.WriteLine("Produto..........:{0,15}",produto);
+        Console.WriteLine("Val.compra.......:{0,15:c}",valcompra);
+        Console.WriteLine("Lucro............:{0,15:p}",lucro);
+        Console.WriteLine("Val.venda........:{0,15:c}",valvenda);
+       }
+
+    }
