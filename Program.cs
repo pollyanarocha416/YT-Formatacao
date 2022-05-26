c6f52ec09ebc47a827e56d13ee7f2232295f5909
Pollyana <pollyanarocha416@gmail.com>
2022-05-26 11:46:50 -0300

    aparencia

--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@
         string produto = "Pastel";
         valvenda = valcompra + (valcompra * lucro);
 
-        Console.WriteLine("Produto..........:{0,15}",produto);
-        Console.WriteLine("Val.compra.......:{0,15:c}",valcompra);
-        Console.WriteLine("Lucro............:{0,15:p}",lucro);
-        Console.WriteLine("Val.venda........:{0,15:c}",valvenda);
+        Console.WriteLine("Produto..........{0,15}",produto);
+        Console.WriteLine("Val.compra.......{0,15:c}",valcompra);
+        Console.WriteLine("Lucro............{0,15:p}",lucro);
+        Console.WriteLine("Val.venda........{0,15:c}",valvenda);
        }
 
     }
